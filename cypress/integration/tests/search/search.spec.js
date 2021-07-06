a6d8a13260a76a54dd7f984d5b3df91d53924366
martin <skarbala.martin@gmail.com>
2021-07-06 14:44:09 +0200

    add search test

--- cypress/integration/tests/search/search.spec.js
+++ cypress/integration/tests/search/search.spec.js
@@ -1,64 +1,88 @@
+import SearchPage from "../../../pages/searchPage";
+describe('Search smoke tests', () => {
+    const searchPage = new SearchPage()
 
-it('finds a flight from NYC to LAX for 3 passengers', () => {
-    const searchCriteria = {
-        from: 'New York',
-        to: 'Los Angeles',
-        passengers: 3
-    }
-    cy.setKiwiConsent()
-    cy.setCookie('preferred_currency', 'EUR')
-    cy.intercept('https://api.skypicker.com/umbrella/v2/**')
-        .as('locations')
-    cy.intercept('**SearchReturnItinerariesQuery**').as('search')
+    it('find a return flight from NYC to LAX for 3 passengers', () => {
+        const searchCriteria = {
+            from: 'New York',
+            to: 'Los Angeles',
+            passengers: 3
+        }
+        cy.setKiwiConsent()
+        cy.setCookie('preferred_currency', 'EUR')
+        cy.intercept('https://api.skypicker.com/umbrella/v2/**')
+            .as('locations')
+        cy.intercept('**SearchReturnItinerariesQuery**').as('search')
+        cy.visit('/')
+        searchPage.clearOriginInput();
+        //zadam kam
+        searchPage.enterOrigin(searchCriteria.from);
+        searchPage.enterDestination(searchCriteria.to);
+        searchPage.addPassengers(searchCriteria.passengers - 1);
 
-    cy.visit('/')
-    cy.get('[data-test="SearchPlaceField-origin"]')
-        .find('[data-test="PlacePickerInputPlace"]')
-        .first()
-        .find('[data-test="PlacePickerInputPlace-close"]')
-        .click()
-    //zadam kam
-    cy.get('[data-test=PlacePickerInput-origin] > [data-test=SearchField-input]')
-        .type(searchCriteria.from)
-    cy.wait('@locations')
-    cy.get('[data-test="PlacepickerModalOpened-origin"]')
-        .contains(searchCriteria.from)
-        .click()
-    cy.get('[data-test=PlacePickerInput-destination] > [data-test=SearchField-input]')
-        .type(searchCriteria.to)
-    cy.wait('@locations')
-    cy.get('[data-test="PlacepickerModalOpened-destination"]')
-        .contains(searchCriteria.to)
-        .click()
+        cy.get('[data-test=LandingSearchButton]')
+            .click()
+        cy.wait('@search')
+        cy.url()
+            .should('include', 'new-york')
+            .and('include', 'los-angeles')
 
-    addPassengers(searchCriteria.passengers - 1);
-
-    cy.get('[data-test=LandingSearchButton]')
-        .click()
-    cy.wait('@search')
-    cy.url()
-        .should('include', 'new-york')
-        .and('include', 'los-angeles')
-
-    cy.get('[data-test="ResultCardWrapper"]')
-        .should('be.visible')
-        .should('have.length.greaterThan', 0)
-        .first().within(() => {
-            cy.get('[data-test="ResultCardPrice"]').should('contain', '€').then(element => {
-                const price = element.text()
-                expect(parseInt(price)).to.be.gt(10);
-                expect(parseInt(price)).to.be.lt(400);
+        cy.get('[data-test="ResultCardWrapper"]')
+            .should('be.visible')
+            .should('have.length.greaterThan', 0)
+            .first().within(() => {
+                cy.get('[data-test="ResultCardPrice"]').should('contain', '€').then(element => {
+                    const price = element.text()
+                    expect(parseInt(price)).to.be.gt(10);
+                    expect(parseInt(price)).to.be.lt(400);
+                })
             })
-        })
-});
+    });
 
-function addPassengers(numberOfPassengers) {
-    cy.get('[data-test="PassengersField"]')
-        .click();
-    cy.get('[data-test=PassengersRow-adults]')
-        .find('button[aria-label="increment"]')
-        .as('addAdult');
-    for (let index = 0; index < numberOfPassengers; index++) {
-        cy.get('@addAdult').click();
-    }
-}
+    it('find a one way flight from Sydney to Melbourne for 1 passenger and 1 cabin bag', () => {
+        const searchCriteria = {
+            from: 'Sydney',
+            to: 'Melbourne',
+            passengers: 1
+        }
+        cy.setKiwiConsent()
+        cy.setCookie('preferred_currency', 'EUR')
+        cy.intercept('https://api.skypicker.com/umbrella/v2/**')
+            .as('locations')
+        cy.intercept('**SearchReturnItinerariesQuery**').as('search')
+        cy.visit('/')
+        searchPage.clearOriginInput();
+        //zadam kam
+        searchPage.enterOrigin(searchCriteria.from);
+        searchPage.enterDestination(searchCriteria.to);
+        searchPage.addPassengers(searchCriteria.passengers - 1);
+        cy.get('[data-test="SearchFormModesPicker-active-return"]').click()
+        cy.get('[data-test="ModePopupOption-oneWay"]').click()
+        cy.get('[data-test="PassengersField"]')
+            .click();
+        cy.get('[data-test="BagsPopup-cabin"]')
+            .find('button[aria-label="increment"]')
+            .click()
+        cy.get('[data-test=LandingSearchButton]')
+            .click()
+        cy.wait('@search')
+        cy.url()
+            .should('include', 'sydney-new-south-wales-australia')
+            .and('include', 'melbourne-victoria-australia')
+
+        cy.get('[data-test="ResultCardWrapper"]')
+            .should('be.visible')
+            .should('have.length.greaterThan', 0)
+            .first().within(() => {
+                cy.get('[data-test="ResultCardPrice"]').should('contain', '€').then(element => {
+                    const price = element.text()
+                    expect(parseInt(price)).to.be.gt(10);
+                    expect(parseInt(price)).to.be.lt(400);
+                })
+            })
+    });
+})
+
+
+
+
